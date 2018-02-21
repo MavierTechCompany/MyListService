d37989af8a65da7e4d46937f96310734d1d3bf47
Michał B <cyberwolf_96@outlook.com>
2018-02-21 16:33:04 +0100

    Added DBContext and one model

---
+++ MyListService/Business Logic/Repositories/DAL/MyListContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyListService.Business_Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyListService.Business_Logic.Repositories.DAL
+{
+    public class MyListContext : DbContext
+    {
+        public MyListContext(DbContextOptions<MyListContext> options)
+            : base(options)
+        { }
+
+        public DbSet<ThingOnList> ThingsOnList { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("MyList");
+
+            modelBuilder.Entity<ThingOnList>(x =>
+            {
+                x.Property(z => z.Id).IsRequired();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
